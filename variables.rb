133fd00513bf202cde6f4f8202d5abbc0ca35323
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-13 00:02:41 +0530

    basic datatypes

--- variables.rb
+++ variables.rb
@@ -1,5 +1,8 @@
 string_age = "34"
 string_name = "roy"
 numeric_age = 34
+double_gpa = 9.0
+is_male = true
+flaws = nil #works like null
 puts (string_name + " is " + string_age + " years old");
 # puts (string_name + " is " + numeric_age + " years old"); //
