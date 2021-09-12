7e6a4624b752f7c6741fb9763dc810a653c24bfa
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-13 01:29:41 +0530

    basic calculator can now handle float values

--- basicAddCalculator.rb
+++ basicAddCalculator.rb
@@ -1,6 +1,6 @@
 puts ("Enter num 1 to be added")
-a = gets().chomp().to_i();
+a = gets().chomp().to_f();
 puts ("Enter num 2 to be added")
-b = gets().chomp().to_i();
+b = gets().chomp().to_f();
 sum = a+b
 puts ("sum is "+sum.to_s())
