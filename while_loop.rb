d88013c6a2ab73a0fe2c7e9b52309e1dab7b4cb7
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-14 00:58:56 +0530

    while loop study in ruby

---
+++ while_loop.rb
@@ -0,0 +1,8 @@
+index = 8;
+#while loop
+while index!=0
+    puts "curr value is "+index.to_s();
+    index -= 1;
+    #index--; # this doesn't works in ruby. we dont have -- and ++ operatores in ruby
+end
+
