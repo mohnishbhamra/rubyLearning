3fdab4248b332ad7ae0b188b81acf01a3bc01ab4
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-14 00:52:30 +0530

    switch case study in ruby

---
+++ switchCaseStudy.rb
@@ -0,0 +1,36 @@
+def whatDayIsIT (abrivation)
+    fullDayName = nil;
+    
+    case abrivation
+    when "mon"
+        fullDayName = "Monday"
+    when "tue"
+        fullDayName = "Tuesday"
+        print("do we need break here if this case doesn't get its run")
+    when "wed"
+        fullDayName = "Wednesday"
+    else         
+        fullDayName = "we dont have any other days apart from mon tue wed :D lol"
+    end
+
+    return fullDayName
+end
+
+
+def switchSomething(data)
+    case data
+    when 1..5 #inclusive between like [1,5]
+      puts "It's between 1 and 5"
+    when 6
+      puts "It's 6"
+    when "foo", "bar"
+      puts "It's either foo or bar"
+    when String #String class
+      puts "You passed a string"
+    else
+      puts "You gave me #{data} -- I have no idea what to do with that."
+    end
+end
+
+#puts whatDayIsIT("wed");
+switchSomething("foot")
