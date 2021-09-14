54993a7fa212732d21b68df5dd2d77480396da3d
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-14 10:41:31 +0530

    handling multiple exceptions

--- exceptionHandling.rb
+++ exceptionHandling.rb
@@ -4,7 +4,13 @@ arr = ["place" ,"city" , "house"];
 
 #arr["place"] actually it is not a map so error
 begin
-arr["place"]
-rescue
-    puts "some handling of exception"
+# arr["place"]
+puts "working line"
+special_zero =  10/0
+arr["place"] #this won't execute as exception caught in prev line
+rescue ZeroDivisionError => error
+    puts "catching just zero division error"
+    puts error.to_s()
+rescue =>e
+    puts "some handling of exception" + e.to_s()
 end
