d9084b637d433936970db21d44081595f2735271
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-15 10:52:41 +0530

    module study in ruby

---
+++ testModule.rb
@@ -0,0 +1,11 @@
+module TestModule
+    def some_method_of_TestModule
+      puts "hi in TestModule"
+    end
+    # puts "hi from outer side"
+    # puts "hi from outer side2"
+    # puts "hi from outer side3"
+end
+
+# puts "hi from outermost side.."
+
