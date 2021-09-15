b300d5389e920051a4c45d5d8011f36f5f437901
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-15 18:26:35 +0530

    instance var and static var

--- variables.rb
+++ variables.rb
@@ -30,3 +30,53 @@ end
 distance, angle = polar(2,2)
 
 puts "distance->"+distance.to_s+", angle->"+angle.to_s
+
+
+$global_var=10
+normal_var=20
+
+class MyClass
+    #try to change global_var and normal_var
+    def initialize
+        puts "initialize of MyClass"
+        normal_var=30
+        $global_var=30
+    end
+
+    #trying to define instance level var
+    def instance_var
+        @instance_var
+    end
+    
+    def instance_var=(str)
+    @instance_var = str
+    end
+
+    def static_var
+        @@static_var
+    end
+
+    def static_var=(str)
+        @@static_var = str
+        end
+
+end
+
+puts "global_var= "+$global_var.to_s+" normal_var= "+normal_var.to_s
+myClassObj = MyClass.new()
+puts "global_var= "+$global_var.to_s+" normal_var= "+normal_var.to_s #notice that normal_var value is still 20, meaning scope of normal_var inside MyClass is limited
+
+
+myClassObj.instance_var = 25;
+myClassObj.static_var = 3675;
+
+myClassObj2 = MyClass.new()
+myClassObj2.instance_var = 35;
+myClassObj2.static_var = 8476;
+
+
+puts "for obj1 instance var is ->"+myClassObj.instance_var.to_s
+puts "for obj2 instance var is ->"+myClassObj2.instance_var.to_s
+
+puts "for obj1 static var is ->"+myClassObj.static_var.to_s
+puts "for obj2 static var is ->"+myClassObj2.static_var.to_s
