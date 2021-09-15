cdc9be73cc1db33b4c962174384f595ad17e9f1e
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-15 17:46:32 +0530

    each iterable in maps

--- mapStudy.rb
+++ mapStudy.rb
@@ -9,3 +9,15 @@ puts(map);
 puts ("specific key value from map is "+ map["key2"]);
 #puts ("specific key value from map is "+ map["randomKey"]); #code broke as the value is nil
 puts(map[12]);
+
+
+h = { # A hash that maps number names to digits
+:nil => 1, # The "arrows" show mappings: key=>value
+:two => 2 # The colons indicate Symbol literals
+}
+
+puts "one's val"+h[:one].to_s # => 1. Access a value by key
+puts h[:three] = 3 # Add a new key/value pair to the hash
+h.each do |key,value| # Iterate through the key/value pairs
+print "#{key}:#{value}; " # Note variables substituted into string
+end
