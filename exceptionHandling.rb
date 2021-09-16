a07f2ca9fa3373c19eeb8dcdea13fb4c02863277
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-17 02:00:54 +0530

    exception handling else, ensure, rescue as modifier

--- exceptionHandling.rb
+++ exceptionHandling.rb
@@ -14,3 +14,26 @@ rescue ZeroDivisionError => error
 rescue =>e
     puts "some handling of exception" + e.to_s()
 end
+
+
+# rescue As a Statement Modifier
+p y = factorial(x) rescue 4
+
+#else , ensure
+def method_name(x)
+    # The body of the method goes here.
+    # Usually, the method body runs to completion without exceptions
+    # and returns to its caller normally.
+    rescue
+    # Exception-handling code goes here.
+    # If an exception is raised within the body of the method, or if
+    # one of the methods it calls raises an exception, then control
+    # jumps to this block.
+    else
+    # If no exceptions occur in the body of the method
+    # then the code in this clause is executed.
+    ensure
+    # The code in this clause is executed no matter what happens in the
+    # body of the method. It is run if the method runs to completion, if
+    # it throws an exception, or if it executes a return statement.
+    end
