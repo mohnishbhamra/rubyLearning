7d731184108aad77d813cf7192ac726c817cdac3
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-16 20:25:48 +0530

    symbols are immutable

--- diagrams/symnbols.rb
+++ diagrams/symnbols.rb
@@ -20,3 +20,14 @@ sym = str.intern # Convert to a symbol
 sym = str.to_sym # Another way to do the same thing
 str = sym.to_s # Convert back to a string
 str = sym.id2name # Another way to do it
+
+
+# Two distinct Symbol objects will always have different
+# content.
+
+# Comparing two Symbol objects for
+# equality is much faster than comparing two strings for equality. For this reason, symbols
+# are generally preferred to strings as hash keys.
+#e.g
+"AM" == "PM" #is slow as strings are mutable and core ruby checks each and every char in string
+:AM == :PM #is fast as symbols are immutable
