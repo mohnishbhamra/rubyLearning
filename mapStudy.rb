23c3e00513d37e06611aba8aa7fc97e0f487ae6b
mohnishbhamra <mohnishsinghbhamra@google.com>
2021-09-16 19:55:13 +0530

    js way is accepted in ruby 1.9

--- mapStudy.rb
+++ mapStudy.rb
@@ -28,3 +28,6 @@ numbers["one"] = 1 # Map the String "one" to the Fixnum 1
 numbers["two"] = 2 # Note that we are using array notation here
 numbers["three"] = 3
 sum = numbers["one"] + numbers["two"] # Retrieve values like this
+
+
+numbers = { one: 1, two: 2, three: 3 } #js way is accepted in ruby 1.9
